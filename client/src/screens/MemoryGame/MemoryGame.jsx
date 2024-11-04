c2b2741bd280127146db59748606f44cfa59634f
Aakash Nautiyal <nautiyalaakash23@gmail.com>
2024-11-05 00:30:02 +0530

    Added circle box container

--- client/src/screens/MemoryGame/MemoryGame.jsx
+++ client/src/screens/MemoryGame/MemoryGame.jsx
@@ -5,6 +5,8 @@ export default function MemoryGame() {
   const divRef = useRef(null); // Create a ref for the div
   const footerRef = useRef(null); // Create a ref for the div
 
+  const [items, setItems] = useState([]);
+  const [itemCount, setItemCount] = useState(4);
   const [headerHeight, setHeaderHeight] = useState(0); // State to store the height
   const [footerHeight, setFooterHeight] = useState(0); // State to store the height
 
@@ -15,19 +17,25 @@ export default function MemoryGame() {
     if (footerRef.current) {
       setFooterHeight(footerRef.current.offsetHeight); // Set the height from the ref
     }
-  }, []);
+    const res = Array.from({ length: itemCount }, (v, i) => ({
+      id: i,
+      visible: false, // Example: alternate visibility
+    }));
+    setItems(res);
+  }, [itemCount]);
 
+  
   return (
     <div className="parentMGameWrapper">
       <div className="header" ref={divRef}>
         <div className="headerContent">
-          <div className="mGameTitle">Fun Stackasd</div>
+          <div className="mGameTitle">Fun Stack</div>
           <div className="mGameHeaderButtonWrapper">
             <button className="newButtonMGame">New game</button>
             <select id="options" className="styled-select">
-              <option value="option1">Option 1</option>
-              <option value="option2">Option 2</option>
-              <option value="option3">Option 3</option>
+              <option value="option1">2 * 2</option>
+              <option value="option2">3 * 3</option>
+              <option value="option3">4 * 4</option>
             </select>
           </div>
         </div>
@@ -39,7 +47,9 @@ export default function MemoryGame() {
           paddingBottom: `${footerHeight}px`,
         }}
       >
-        Body
+        <div className="common-flex-box">
+          <div className="circleBox">Body</div>
+        </div>
       </div>
       <div className="footer" ref={footerRef}>
         Footer
