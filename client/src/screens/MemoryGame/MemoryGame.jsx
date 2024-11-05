3169ca4fc209665e1596b3d350825a8fad669d62
Aakash Nautiyal <nautiyalaakash23@gmail.com>
2024-11-05 23:48:17 +0530

    Added grid functionality

--- client/src/screens/MemoryGame/MemoryGame.jsx
+++ client/src/screens/MemoryGame/MemoryGame.jsx
@@ -6,9 +6,20 @@ export default function MemoryGame() {
   const footerRef = useRef(null); // Create a ref for the div
 
   const [items, setItems] = useState([]);
-  const [itemCount, setItemCount] = useState(4);
+  const [itemCount, setItemCount] = useState(64);
   const [headerHeight, setHeaderHeight] = useState(0); // State to store the height
   const [footerHeight, setFooterHeight] = useState(0); // State to store the height
+  const [options, setOptions] = useState([
+    {
+      label: "2 * 2",
+      value: 2,
+    },
+    {
+      label: "4 * 4",
+      value: 4,
+    },
+  ]);
+  const [dropdownValue, setDropdownValue] = useState(2); // State to store the height
 
   useEffect(() => {
     if (divRef.current) {
@@ -24,7 +35,24 @@ export default function MemoryGame() {
     setItems(res);
   }, [itemCount]);
 
-  
+   useEffect(() => {setItemCount(dropdownValue * dropdownValue);}, [dropdownValue]);
+
+  const circularStructure = () => <div className="mGameCircle" />;
+
+  const bodyStruture = () => {
+    return (
+      <div
+        className="gridBodyStructure"
+        style={{
+          gridTemplateColumns: `repeat(${Math.sqrt(items.length)}, 1fr)`,
+        }}
+      >
+        {items.map((ele, index) => (
+          <div key={`${index}${ele.id}`}>{circularStructure()}</div>
+        ))}
+      </div>
+    );
+  };
   return (
     <div className="parentMGameWrapper">
       <div className="header" ref={divRef}>
@@ -32,10 +60,16 @@ export default function MemoryGame() {
           <div className="mGameTitle">Fun Stack</div>
           <div className="mGameHeaderButtonWrapper">
             <button className="newButtonMGame">New game</button>
-            <select id="options" className="styled-select">
-              <option value="option1">2 * 2</option>
-              <option value="option2">3 * 3</option>
-              <option value="option3">4 * 4</option>
+            <select
+              id="options"
+              className="styled-select"
+              onChange={(e) => setDropdownValue(e.target.value)}
+            >
+              {options.map((ele, index) => (
+                <option key={`${index}${ele.value}`} value={ele.value}>
+                  {ele.label}
+                </option>
+              ))}
             </select>
           </div>
         </div>
@@ -47,8 +81,8 @@ export default function MemoryGame() {
           paddingBottom: `${footerHeight}px`,
         }}
       >
-        <div className="common-flex-box">
-          <div className="circleBox">Body</div>
+        <div className="common-flex-box" style={{ height: "100%" }}>
+          <div className="circleBox">{bodyStruture()}</div>
         </div>
       </div>
       <div className="footer" ref={footerRef}>
