49e85fe6d891a03790e9174c1deda5815c0d3025
Aditya Nautiyal <aditya.nautiyal97@gmail.com>
2025-08-01 23:25:22 +0530

    Add schema documentation comments to model files
    Added comments to Employee, ScoreForFour, and ScoreForSix model files to clarify schema definitions and their usage for MongoDB collections.

--- server/models/Employee.js
+++ server/models/Employee.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+//Defining the schema for Employee
+//This schema will be used to create a model for the Employee collection in MongoDB
 const EmployeeSchema = new mongoose.Schema({
     email: String,
     password: String
--- server/models/ScoreForSix.js
+++ server/models/ScoreForSix.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+// Defining the schema for ScoreForSix
+// This schema will be used to create a model for the ScoreForFour collection in MongoDB
 const ScoreSchema = new mongoose.Schema({
   email: String,
   score: String,
